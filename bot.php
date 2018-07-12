48636afb1b5d11a39200ec4fdfd24973cc49a1bc
bares12 <40828949+bares12@users.noreply.github.com>
2018-07-12 14:36:42 +0000

    Update bot.php

--- bot.php
+++ bot.php
@@ -5,8 +5,8 @@ Support by : Mastah Ervan
 */
 require_once('./line_class.php');
 require_once('./unirest-php-master/src/Unirest.php');
-$channelAccessToken = '0lpuUiIxUbiuW/FT+o692TeUWINEAcQ6OMB6AwNEsQEk29ErLyyCFSglnkL7hZkybK7pTMoqgG8GCXFjYRlKO4IvPiebZGmzOzl5yWri74mdxQRlZxHU8Wr7htvNP/ZJ+3IsadRaJAHZXjjzU1djgwdB04t89/1O/w1cDnyilFU='; #ChannelAccessToken (paste token akses kamu disini)
-$channelSecret = '99cc4ccfd8bcbd0c2059306fa026b22a';#Channel Secret (paste token secret kamu disini)
+$channelAccessToken = 'W87tpLbjGorG1Oinv3DWM8XdNriJ2NsCmnos6VaI6D5obHTIM6NkC/UUMN24XdpAduwc5YDuFV45gQqRxVt3Ibu1O4CgRbCNJU+lru5RumhP0vYeFMgtycbiNOz3gQGwsNgGjXloAaqV1rj5S4ma0QdB04t89/1O/w1cDnyilFU='; #ChannelAccessToken (paste token akses kamu disini)
+$channelSecret = '941b173d5a8b59b29b2bc1d00657f826';#Channel Secret (paste token secret kamu disini)
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 $userId 	= $client->parseEvents()[0]['source']['userId'];
 $groupId 	= $client->parseEvents()[0]['source']['groupId'];
